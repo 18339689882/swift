371704f7283fabe1c87844151f463188d23d9618
My MacBook Pro <17319461124@163.com>
2019-12-10 20:32:27 +0800

    创建playGround

--- playGround.playground/Contents.swift
+++ playGround.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var str = "Hello, playground"
+let maxNumber = 10
+var currentNum = 20
+
+//maxNumber = 5  //常量不能直接赋值。报错
+
+currentNum = 1 //变量可以赋值。
+
+// 一行定义多个常量 或者变量
+let x = 0.0, y = 0.0, z = "123"
+
+print("x = \(x), y =\(y), z = \(z)")
+
+let ab = "abc"
