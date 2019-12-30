ac1a25ca20d345f090c817ed2f5d182a273e0996
Devin <2418175743@qq.com>
2019-12-30 17:32:09 +0800

    运算符

--- SHswift/SHswift/ViewController.swift
+++ SHswift/SHswift/ViewController.swift
@@ -12,42 +12,88 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let maximumNumberOfLoginAttempts = 10
-        var currentLoginAttempt = 0
+//        let maximumNumberOfLoginAttempts = 10
+//        var currentLoginAttempt = 0
+//
+//        var x = 0.0, y = 0.0, z = 0.0
+//
+//        var weclomeMessage: String
+//        weclomeMessage = "swift"
+//
+//        var red, blue, green: Double
+//
+//        //MARK:常量和变量名可以包含任何字符
+//        let π = 3.14159
+//        let 你好 = "你好世界"
+//        let 🐶🐮 = "dogcow"
+//
+//        //MARK:变量值可以改变
+//        var friendlyWelcome = "Hello!"
+//        friendlyWelcome = "Bonjour!"
+//        // friendlyWelcome 现在是 "Bonjour!"
+//
+//        //MARK:常量则不行
+//        let friendlyWelcome1 = "Hello!"
+////        friendlyWelcome1 = "Bonjour!"（报错）
+//
+//        //输出
+//        print(friendlyWelcome1)
+//
+//        MARK:变量或者常量输出。类似于oc NSLog(@"%@", friendlyWelcome)
+//        print("The current value of friendlyWelcome is \(friendlyWelcome)")
+//        // 输出“The current value of friendlyWelcome is Bonjour!”
+//
+//        MARK:分号 可以不加， 一行写多个语句，必须加分号。
+//        let cat = "🐱"; print(cat)
+//        // 输出“🐱”
         
-        var x = 0.0, y = 0.0, z = 0.0
+//        // MARK: - 实例方法 断言
+//        let age = -3
+//
+//        assert(age >= 0, "A person's age cannot be less than zero")
+//
+//        if age > 10 {
+//            print("You can ride the roller-coaster or the ferris wheel.")
+//        } else if age > 0 {
+//            print("You can ride the ferris wheel.")
+//        } else {
+//            assertionFailure("A person's age can't be less than zero.")
+//        }
+//        // 在一个下标的实现里...
+//        let index = -1;
+//        precondition(index > 0, "Index must be greater than zero.")
+//        //等价 if
+//
+//        if let firstNumber = Int("4"), let secondNumber = Int("42"), firstNumber < secondNumber && secondNumber < 100 {
+//            print("\(firstNumber) < \(secondNumber) < 100")
+//        }
+//        // 输出“4 < 42 < 100”
+//
+//        if let firstNumber = Int("4") {
+//            if let secondNumber = Int("42") {
+//                if firstNumber < secondNumber && secondNumber < 100 {
+//                    print("\(firstNumber) < \(secondNumber) < 100")
+//                }
+//            }
+//        }
+        // 输出“4 < 42 < 100”
         
-        var weclomeMessage: String
-        weclomeMessage = "swift"
+        //MARK: 元组赋值
+//        let (x, y) = (1, 2)
+        // 现在 x 等于 1，y 等于 2
         
-        var red, blue, green: Double
+        //MARK: + 用于字符串的拼接
+//        "hello, " + "world"  // 等于 "hello, world"
+        //元组 之间的 符号判断
+//        (1, "zebra") < (2, "apple")   // true，因为 1 小于 2
+//        (3, "apple") < (3, "bird")    // true，因为 3 等于 3，但是 apple 小于 bird
+//        (4, "dog") == (4, "dog")      // true，因为 4 等于 4，dog 等于 dog
         
-        //常量和变量名可以包含任何字符
-        let π = 3.14159
-        let 你好 = "你好世界"
-        let 🐶🐮 = "dogcow"
+        // Bool 不能被比较，也意味着存有布尔类型的元组不能被比较。
         
-        //变量值可以改变
-        var friendlyWelcome = "Hello!"
-        friendlyWelcome = "Bonjour!"
-        // friendlyWelcome 现在是 "Bonjour!"
+        // TODO: TODO
         
-        //常量则不行
-        let friendlyWelcome1 = "Hello!"
-//        friendlyWelcome1 = "Bonjour!"（报错）
-        
-        //输出
-        print(friendlyWelcome1)
-        
-        //变量或者常量输出。类似于oc NSLog(@"%@", friendlyWelcome)
-        print("The current value of friendlyWelcome is \(friendlyWelcome)")
-        // 输出“The current value of friendlyWelcome is Bonjour!”
-        
-        //分号 可以不加， 一行写多个语句，必须加分号。
-        let cat = "🐱"; print(cat)
-        // 输出“🐱”
+        // FIXME:FIXME
     }
-
-
 }
 
